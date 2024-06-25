04bbeac0bc78deb620d33d434c596f772b828ccf
kvp133 <kieuvietphuoc13@gmail.com>
2024-06-26 00:13:24 +0700

    update table

---
+++ BilliardManagement/ViewModels/ProductViewModel.cs
@@ -0,0 +1,107 @@
+﻿using BilliardManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace BilliardManagement.ViewModels
+{
+    public class ProductViewModel : BaseViewModel
+    {
+
+        public ICommand AddCommand { get; set; }      
+        public ICommand EditCommand { get; set; }
+        public ICommand DeleteCommand { get; set; }
+        private ObservableCollection<Product> _List;
+        public ObservableCollection<Product> List { get => _List; set { _List = value; OnPropertyChanged(); } }
+        
+        private string _ProductName;
+        public string ProductName { get => _ProductName; set { _ProductName = value; OnPropertyChanged(); } }
+        private decimal _Price;
+        public decimal Price { get => _Price; set { _Price = value; OnPropertyChanged(); } }
+        private string _Description;
+        public string Description { get => _Description; set { _Description = value; OnPropertyChanged(); } }
+        private string _SearchText;
+        public string SearchText { get => _SearchText; set { _SearchText = value; OnPropertyChanged(); } }
+        private Product _SelectedItem;
+        public Product SelectedItem
+        {
+            get => _SelectedItem; set
+            {
+                _SelectedItem = value;
+                if (SelectedItem != null)
+                {
+                    ProductName = SelectedItem.ProductName;
+                    Price = SelectedItem.Price;
+                    Description = SelectedItem.Description;
+                }
+                OnPropertyChanged();
+            }
+        }
+
+
+
+
+
+        // mọi thứ xử lý sẽ nằm trong này
+        public ProductViewModel()
+        {
+            List = new ObservableCollection<Product>(DataProvider.Instance.DB.Products);
+            AddCommand = new RelayCommand<object>((p) =>
+            {
+                if (string.IsNullOrEmpty(ProductName) || string.IsNullOrEmpty(Price.ToString()))
+                    return false;
+                if (DataProvider.Instance.DB.Products.Where(x => x.ProductName == ProductName).Count() != 0)
+                    return false;
+                return true;
+            }, (p) =>
+            {
+                var product = new Product() { ProductName = ProductName, Price = Price, Description = Description };
+                DataProvider.Instance.DB.Products.Add(product);
+                DataProvider.Instance.DB.SaveChanges();
+                List.Add(product);
+            }
+              );
+            EditCommand = new RelayCommand<object>((p) =>
+            {
+                if (string.IsNullOrEmpty(ProductName) || string.IsNullOrEmpty(Price.ToString()) || SelectedItem == null)
+                    return false;
+                if (DataProvider.Instance.DB.Products.Where(x => x.ProductName == ProductName && x.ProductId != SelectedItem.ProductId).Count() != 0)
+                    return false;
+                return true;
+            }, (p) =>
+            {
+                var product = DataProvider.Instance.DB.Products.Where(x => x.ProductId == SelectedItem.ProductId).FirstOrDefault();
+                product.ProductName = ProductName;
+                product.Price = Price;
+                product.Description = Description;
+                DataProvider.Instance.DB.SaveChanges();
+                SelectedItem.ProductName = ProductName;
+                SelectedItem.Price = Price;
+                SelectedItem.Description = Description;
+                List = new ObservableCollection<Product>(DataProvider.Instance.DB.Products);
+
+            }
+              );
+            DeleteCommand = new RelayCommand<object>((p) =>
+            {
+   return SelectedItem != null; 
+            }, (p) =>
+            {
+                var product = DataProvider.Instance.DB.Products.Where(x => x.ProductId == SelectedItem.ProductId).FirstOrDefault();
+                DataProvider.Instance.DB.BookingDetails.RemoveRange(product.BookingDetails);
+                DataProvider.Instance.DB.Products.Remove(product);
+                DataProvider.Instance.DB.SaveChanges();
+                List.Remove(product);
+            }
+              );
+
+           
+
+        }
+    }
+}
---
+++ BilliardManagement/ViewModels/TableViewModel.cs
@@ -0,0 +1,108 @@
+﻿using BilliardManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BilliardManagement.ViewModels
+{
+    public class TableViewModel : BaseViewModel
+    {
+        public ICommand AddCommand { get; set; }
+        public ICommand EditCommand { get; set; }
+        public ICommand DeleteCommand { get; set; }
+        private ObservableCollection<Table> _List;
+        public ObservableCollection<Table> List { get => _List; set { _List = value; OnPropertyChanged(); } }
+        private string _TableNumber;
+        public string TableNumber { get => _TableNumber; set { _TableNumber = value; OnPropertyChanged(); } }
+        private string _HourlyRate;
+        public string HourlyRate { get => _HourlyRate; set { _HourlyRate = value; OnPropertyChanged(); } }
+        private string _Status;
+        public string Status { get => _Status; set { _Status = value; OnPropertyChanged(); } }
+        private string _SearchText;
+        public string SearchText { get => _SearchText; set { _SearchText = value; OnPropertyChanged(); } }
+        private string _StatusSelected;
+        public string StatusSelected { get => _StatusSelected; set { _StatusSelected = value; OnPropertyChanged(); } }
+        private ObservableCollection<string> _ListStatus;
+        public ObservableCollection<string> ListStatus
+        {
+            get => _ListStatus;
+            set
+            {
+                _ListStatus = value;
+                OnPropertyChanged();
+            }
+        }
+        private Table _SelectedItem;
+        public Table SelectedItem
+        {
+            get => _SelectedItem; set
+            {
+                _SelectedItem = value;
+                if (SelectedItem != null)
+                {
+                    TableNumber = SelectedItem.TableNumber.ToString();
+                    HourlyRate = SelectedItem.HourlyRate.ToString();
+                    Status = SelectedItem.Status;
+                    StatusSelected = SelectedItem.Status;
+                }
+                OnPropertyChanged();
+            }
+        }
+
+        public TableViewModel() 
+        {
+            List = new ObservableCollection<Table>(DataProvider.Instance.DB.Tables);
+            ListStatus = new ObservableCollection<string>() { "Available", "Occupied", "Maintenance" };
+            AddCommand = new RelayCommand<object>((p) =>
+            {
+                if (TableNumber == null || HourlyRate == null)
+                    return false;
+                if (DataProvider.Instance.DB.Tables.Where(x => x.TableNumber == int.Parse(TableNumber)).Count() != 0)
+                    return false;
+                return true;
+            }, (p) =>
+            {
+                var table = new Table() { TableNumber = int.Parse(TableNumber), HourlyRate = decimal.Parse(HourlyRate) , Status = "Available" };
+                DataProvider.Instance.DB.Tables.Add(table);
+                DataProvider.Instance.DB.SaveChanges();
+                List.Add(table);
+            });
+            EditCommand = new RelayCommand<object>((p) =>
+            {
+                if (TableNumber == null || HourlyRate == null || SelectedItem == null)
+                    return false;
+                var displayList = DataProvider.Instance.DB.Tables.Where(x => x.TableNumber == int.Parse(TableNumber));
+                if (displayList.Count() != 0 && displayList.First().TableId != SelectedItem.TableId)
+                    return false;
+                return true;
+            }, (p) =>
+            {
+                var table = DataProvider.Instance.DB.Tables.Where(x => x.TableId == SelectedItem.TableId).SingleOrDefault();
+                table.TableNumber = int.Parse(TableNumber);
+                table.HourlyRate = Convert.ToDecimal(HourlyRate);
+                DataProvider.Instance.DB.SaveChanges();
+            SelectedItem.TableNumber = int.Parse(TableNumber);
+                SelectedItem.HourlyRate = decimal.Parse(HourlyRate);
+                List = new ObservableCollection<Table>(DataProvider.Instance.DB.Tables);
+            });
+            DeleteCommand = new RelayCommand<object>((p) =>
+            {
+                if (SelectedItem == null)
+                    return false;
+                return true;
+            }, (p) =>
+            {
+                var table = DataProvider.Instance.DB.Tables.Where(x => x.TableId == SelectedItem.TableId).SingleOrDefault();
+                DataProvider.Instance.DB.Bookings.RemoveRange(table.Bookings);
+                DataProvider.Instance.DB.Tables.Remove(table);
+                DataProvider.Instance.DB.SaveChanges();
+                List.Remove(table);
+            });
+        
+        }
+    }
+}
